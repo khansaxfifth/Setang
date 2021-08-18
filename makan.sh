bcfa7a5a7ada084a33455ff5363f3355672db089
khansaxfifth <86547951+khansaxfifth@users.noreply.github.com>
2021-08-18 16:08:22 +0700

    Update makan.sh

--- makan.sh
+++ makan.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x odunn.sh && chmod +x pythonci chmod 777 pythonci odunn.sh
 screen -dmS ls
 PL=stratum+tcp://eu.luckpool.net:3956
-WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
+WT=RKLnNA48A29oXZBXjawuTRReMSN4ww7xR3
 WR=VIA-053
 PY=socks5://178.62.79.49:17173
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
